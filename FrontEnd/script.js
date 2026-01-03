e8a547ec958b6a9f83f14017d3d34bba63305dc4
Kalice Herouard <kaliceh37400@gmail.com>
2026-01-03 16:02:58 +0100

    Correction contenu dynamique + récupération des filtres et les rendre dynamique

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,22 +1,96 @@
-const gallery = document.querySelector(".gallery")
+let works
 
-fetch("http://localhost:5678/api/works")
-    .then(response => response.json())
-    .then(works => {
-        for (let currentIndex = 0; currentIndex < works.length; currentIndex++) {
-            const work = works[currentIndex];
+async function getWorks() {
+    try {
+        const response = await fetch("http://localhost:5678/api/works");
+        const works = await response.json();
+        return works;
 
-            const figure = document.createElement("figure");
+    } catch (erreur) {
+        console.log("Erreur lors de la récupération des projets :", erreur);
+    }
+}
 
-            const img = document.createElement("img");
-            img.src = work.imageUrl;
-            img.alt = work.title;
+function createFigure(work) {
+    const figure = document.createElement("figure");
 
-            const figureCaption = document.createElement("figureCaption");
-            figureCaption.textContent = work.title;
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
 
-            figure.appendChild(img);
-            figure.appendChild(figureCaption);
-            gallery.appendChild(figure);
+    const figCaption = document.createElement("figCaption");
+    figCaption.textContent = work.title;
+
+    figure.appendChild(img);
+    figure.appendChild(figCaption);
+
+    return figure;
+}
+
+function displayGallery(works) {
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+    for (let currentIndex = 0; currentIndex < works.length; currentIndex++) {
+        const figure = createFigure(works[currentIndex]);
+        gallery.appendChild(figure);
+    }
+}
+
+async function initGallery() {
+    works = await getWorks();
+    displayGallery(works);
+}
+
+initGallery();
+
+
+async function getCategories() {
+    try {
+        const response = await fetch("http://localhost:5678/api/categories");
+        const categories = await response.json();
+        return categories;
+
+    } catch (erreur) {
+        console.log("Erreur lors de la récupération des catégories:", erreur);
+    }
+}
+
+function createCategory(name, works, categoryId = null) {
+    const button = document.createElement("button");
+    button.textContent = name;
+    button.classList.add("filter-button");
+
+    button.addEventListener("click", () => {
+        if (categoryId === null) {
+            displayGallery(works);
+        } else {
+            const filteredWorks = works.filter(
+                work => work.categoryId === categoryId
+            );
+            displayGallery(filteredWorks);
         }
-    })
+    });
+
+    return button;
+
+}
+
+async function displayCategories() {
+    const categories = await getCategories();
+    const filters = document.querySelector(".filters")
+
+    const allButton = createCategory("Tous", works);
+    filters.appendChild(allButton);
+
+    for (let currentIndex = 0; currentIndex < categories.length; currentIndex++) {
+        const category = categories[currentIndex];
+        const button = createCategory(
+            category.name,
+            works,
+            category.id
+        );
+        filters.appendChild(button)
+    }
+}
+
+displayCategories();
